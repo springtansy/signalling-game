75957d6d12e29d08a3d8f5b7f1f0a713aa8e8433
springtansy <elvis.blomster+github@gmail.com>
2026-06-30 09:25:15 +0300

    Create track.js

---
+++ track.js
@@ -0,0 +1,14 @@
+class Track {
+  constructor(id,startX,startY,endX,endY,speedLimit=15) {
+    this.id = id
+    this.start = {
+      x = startX,
+      y = startY,
+    }
+    this.end = {
+      x = endX,
+      y = endY,
+    }
+    this.speedLimit = speedLimit
+  }
+}
